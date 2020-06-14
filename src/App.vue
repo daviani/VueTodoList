1db10b2597c0b2a046639ca539cb662c4ae7032f
d.Fillatre81@gmail.com <38353146+daviani@users.noreply.github.com>
2020-06-14 18:43:33 +0200

    First Commit
    Signed-off-by: d.Fillatre81@gmail.com <38353146+daviani@users.noreply.github.com>

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+ <NavBar/>
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+  // Components
+  import NavBar from "./view/components/NavBar/NavBar";
+
+export default {
+  name: 'App',
+  components: {
+      NavBar
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+</style>
---
+++ src/view/components/NavBar/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+        <div class="header-top">
+            <h1> <strong>A vue Todo List</strong></h1>
+        </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'navbar',
+        props: {
+        },
+    };
+</script>
+
+<style scoped>
+    .header-top {
+        position: fixed;
+        top: 0;
+        height: 50px;
+        width: 100%;
+        background: #41b883;
+        color: #35495e;
+        text-align: center;
+        line-height: 50px;
+        font-size: 25px;
+    }
+</style>
